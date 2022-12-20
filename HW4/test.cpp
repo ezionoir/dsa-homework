a71c0f8dacfcd3dd4da541064f037a4bdc84092c
huynhlonguni <106131819+huynhlonguni@users.noreply.github.com>
2022-12-20 20:11:20 +0700

    HW4

---
+++ HW4/test.cpp
@@ -0,0 +1,56 @@
+#include "circular_linked_list.h"
+#include <iostream>
+using namespace std;
+
+int menuCmd(List *L, int option) {
+	int data, val;
+	switch (option) {
+		case 0:
+			return 0;
+		case 1:
+			cout << "Data = ";
+			cin >> data;
+			addHead(L, data);
+			break;
+		case 2:
+			cout << "Data = ";
+			cin >> data;
+			addTail(L, data);
+			break;
+		case 3:
+			removeHead(L);
+			break;
+		case 4:
+			removeTail(L);
+			break;
+		case 5:
+			cout << "Data = ";
+			cin >> data;
+			cout << "Val = ";
+			cin >> val;
+			addAfter(L, data, val);
+			break;
+		case 6:
+			cout << "Val = ";
+			cin >> val;
+			removeAfter(L, val);
+			break;
+	}
+	printList(L);
+	return 1;
+}
+ 
+int main() {
+	List *L = createList();
+	int option = 0;
+	do {
+		cout << "0: Quit" << endl;
+		cout << "1: addHead" << endl;
+		cout << "2: addTail" << endl;
+		cout << "3: removeHead" << endl;
+		cout << "4: removeTail" << endl;
+		cout << "5: addAfter" << endl;
+		cout << "6: removeAfter" << endl;
+		cin >> option;
+	} while (menuCmd(L, option));
+}
