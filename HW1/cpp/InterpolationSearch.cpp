556b4904df9ac35f62f9f672b96e6413ba270d50
ezionoir <tohienvinh@gmail.com>
2022-12-29 19:08:58 +0700

    chac la xong roi

--- HW1/cpp/InterpolationSearch.cpp
+++ HW1/cpp/InterpolationSearch.cpp
@@ -7,7 +7,11 @@ int InterpolationSearch(int a[], int n, int k, int& loopCount) {
     int left = 0, right = n - 1;
     loopCount = 0;
 
+    // If all conditions are sastified, then start a new loop
     while ((left <= right) && (k >= a[left]) && (k <= a[right]) && (++loopCount)) {
+        //   x - left          right - left
+        // -------------- = ------------------
+        // a[x] - a[left]   a[right] - a[left]
         int x = left + int(floor((right - left) * double(k - a[left]) / double(a[right] - a[left])));
         if (a[x] == k) return x;
         if (a[x] < k) left = x + 1;
