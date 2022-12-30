34a94b08d9fad0eb7f08bdb041e59e9c4159b839
ezionoir <tohienvinh@gmail.com>
2022-12-30 17:00:57 +0700

    xong

--- HW9/CommandProcess.cpp
+++ HW9/CommandProcess.cpp
@@ -1,5 +1,6 @@
 #include "./CommandProcess.h"
 
+// Convert a non-negative integer into binary string with nbits bits
 string ToBinary(int x, int nbits) {
     stringstream writer;
     for (int i = nbits - 1; i >= 0; --i) {
@@ -10,10 +11,12 @@ string ToBinary(int x, int nbits) {
 }
 
 void CommandProcess(int argc, char** argv) {
+    // Compress
     if (strcmp(argv[1], "-c") == 0) {
         string input = argv[2];
         vector<int> output = encode(input);
 
+        // Print the output
         for (int i = 0; i < output.size(); ++i)
             cout << output[i] << " ";
         cout << endl;
@@ -22,23 +25,31 @@ void CommandProcess(int argc, char** argv) {
             cout << ToBinary(output[i], CHAR_SIZE) << " ";
         cout << endl;
 
+        // Print compress rate
         long long int N = sizeof(char) * 8 * input.length();
         long long int n = CHAR_SIZE * output.size();
         double compressRate = (double(N) - double(n)) / double(N) * 100;
         cout << fixed << setprecision(2) << compressRate << endl;
+
+        return;
     }
 
+    // Extract
     if (strcmp(argv[1], "-e") == 0) {
         vector<int> input;
         for (int i = 2; i < argc; ++i)
             input.push_back(atoi(argv[i]));
 
+        // Print the output
         string output = decode(input);
         cout << output << endl;
 
+        // Print compress rate
         long long int N = sizeof(char) * 8 * output.length();
         long long int n = CHAR_SIZE * input.size();
         double compressRate = (double(N) - double(n)) / double(N) * 100;
         cout << fixed << setprecision(2) << compressRate << endl;
+
+        return;
     }
 }
--- HW9/LZW.cpp
+++ HW9/LZW.cpp
@@ -1,22 +1,35 @@
+// https://www.geeksforgeeks.org/lzw-lempel-ziv-welch-compression-technique/
 #include "./LZW.h"
 
+// Notation: We call a "character" is for a string that is added to the current dictionary, character is for a single character ('a', 'b', ...)
+
 vector<int> encode(string input) {
+    // We map a "character" with its ID (the order that it is added to the dictionary/table)
+    // Use unorder map for faster look up
     unordered_map<string, int> table;
+
+    // Every single character is added first
     for (int i = 0; i < 256; ++i)
         table[string(1, char(i))] = i;
 
     string prefix(1, char(input[0])), appChar = "";
     vector<int> output;
-    int code = 256;
+    int code = 256; // The current ID for next "character"
 
     for (int i = 1; i < input.length(); ++i) {
         appChar += input[i];
 
+        // Look up the "character" generate by prefix + the next character in input
+        // If it appears in table, then we continue to build longer "character" (current "character" becomes a prefix and we append it with the next character in input)
         if (table.find(prefix + appChar) != table.end())
             prefix += appChar;
+        // If it does not appear in table, then it is a new "character"
         else {
+            // Since it's new, we can just output the prefix (old "character")
             output.push_back(table[prefix]);
+            // Add the new "character" to table
             table[prefix + appChar] = code++;
+            // Reset the prefix (set it by the new character)
             prefix = appChar;
         }
         appChar = "";
@@ -28,6 +41,7 @@ vector<int> encode(string input) {
 
 string decode(vector<int> input) {
     unordered_map<int, string> table;
+    // Every single character is added first
     for (int i = 0; i < 256; ++i)
         table[i] = string(1, char(i));
 
@@ -39,6 +53,7 @@ string decode(vector<int> input) {
     for (int i = 1; i < input.size(); ++i) {
         newID = input[i];
 
+        // If the newID correspond to a "character" added, we just output it
         if (table.find(newID) != table.end())
             s = table[newID];
         else
@@ -46,6 +61,7 @@ string decode(vector<int> input) {
             
         output += s;
         c = string(1, s[0]);
+        // New "character" = previous "character" + next character in input
         table[count++] = table[oldID] + c;
         oldID = newID;
     }
